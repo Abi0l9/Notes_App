ba0d7f3ed90245cf302f53406a54d25c9317a8d3
Abi0l9 <cryptog3ms@gmail.com>
2023-02-16 09:46:08 +0100

    update notes route added

--- Backend/controller/note.js
+++ Backend/controller/note.js
@@ -2,20 +2,37 @@ const noteRouter = require("express").Router();
 const User = require("../models/user");
 const Note = require("../models/note");
 
-noteRouter.get("", (request, response) => response.send("welcome"));
-noteRouter.post("", async (request, response) => {
-  const { title, body } = request.body;
-  const user = await User.findById(request.user);
-  const note = new Note({
-    title,
-    body,
-    date: new Date().toUTCString(),
-    user: user._id,
+noteRouter
+  .get("", async (request, response) => {
+    const notes = await Note.find({}).populate("user", { name: 1 });
+
+    return response.status(200).json(notes).end();
+  })
+
+  .post("", async (request, response) => {
+    const { title, body } = request.body;
+    const user = await User.findById(request.user);
+
+    const note = new Note({
+      title,
+      body,
+      date: new Date().toUTCString(),
+      user: user._id,
+    });
+
+    const savedNote = await note.save();
+
+    user.notes = user.notes.concat(note.id);
+    await user.save();
+
+    return response.status(201).json(note);
+  })
+
+  .patch("/:noteId", async (request, response) => {
+    const noteId = request.params.noteId;
+    const note = await Note.findByIdAndUpdate(noteId, request.body);
+
+    return response.status(200).json({ message: "Edited Successfully" }).end();
   });
-
-  console.log(note);
-
-  return response.json(user);
-});
 
 module.exports = noteRouter;
